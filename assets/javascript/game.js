60d80f75e94b3f1a6a610cd3c70316c9dbacb63c
AmbesaLij <mikemulat@gmail.com>
2018-11-10 10:33:49 -0500

    functioning game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,80 +1,57 @@
-// make an array of words 
-var artists = ["Dali", "picasso", "Monet", "warhol", "kahlo", "munch", "caravaggio", "goya", "basquiat", "Neel", "walker", "wiley", "lawrence", "shinobare"]["Dali", "picasso", "Monet", "warhol", "kahlo", "munch", "caravaggio", "goya", "basquiat", "Neel", "walker", "wiley", "lawrence", "shinobare"];
-var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var numWins = 0;
+var numLosses = 0;
 
-var numWins;
-var numLosses;
-var numGuesses;
 
-// Start the game
-// pick a random word from the array
-var randomNum = (Math.floor(Math.random() * artists.length));
-var wordToGuess = artists[randomNum].toUpperCase();
-console.log("Word to guess: " + wordToGuess)
+window.onload = HangmanGame();
+// Use this functon array to create blank spaces in the HTML #missing-letters
+function HangmanGame(){
 
-// create underscores to represent the missing letters of the word
-var blankWord = [];
-var wrongLetters = [];
+    var numGuesses = 10;
+    // Start the game
+    // pick a random word from the array
+    var artists = ["Dali", "picasso", "Monet", "warhol", "kahlo", "munch", "caravaggio", "goya", "basquiat", "Neel", "walker", "wiley", "lawrence", "shinobare"];
+    var randomNum = (Math.floor(Math.random() * artists.length));
+    var wordToGuess = artists[randomNum].toUpperCase();
+    console.log("Word to guess: " + wordToGuess);
 
-var instructionsElem = document.getElementById("instructions");
-var missingLettersElem = document.getElementById("missing-letters");
-var wrongLettersElem = document.getElementById("wrong_letters");
-var numWinsElem = document.getElementById("win-numbers");
-var numLossesElem = document.getElementById("loss-numbers");
-var numGuessesElem = document.getElementById("try-numbers");
+    var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-function newFunction() {
-    return 10;
-}
+    // create underscores to represent the missing letters of the word
+    var blankWord = [];
+    var wrongLetters = [];
 
-function findIndicies(aWord, character) {
-    var indicies = [], i;
-    for (i = 0; i < aWord.length; i++) {
-        if (aWord[i] === character)
-            indicies.push(i);
-    }
-    return indicies;
-}
-
-// create blank array
-function createBlanks(aString) {
-    for (i = 0; i < aString.length; i++) {
-        blankWord.push("_")
-    }
-    return blankWord
-}
-
-function updateWins(){
     
-}
 
-function updateLosses(){
-    if (numGuesses == 0) {
-        numLosses++;
-        numLossesElem.innerHTML = numLosses;
+    var instructionsElem = document.getElementById("instructions");
+    var missingLettersElem = document.getElementById("missing-letters");
+    var wrongLettersElem = document.getElementById("wrong_letters");
+    var numWinsElem = document.getElementById("win-numbers");
+    var numLossesElem = document.getElementById("loss-numbers");
+    var numGuessesElem = document.getElementById("try-numbers");
+    var lowTriesElem = document.getElementById("try-score-board");
 
-        startGame();
-    }
-}
 
-// Use this functonarray to create blank spaces in the HTML #missing-letters
-function startGame(){
     createBlanks(wordToGuess);
     missingLettersElem.innerHTML = blankWord.join(" ");
 
-    var numGuesses = 10;
-    var 
-}
+    resetUI();
 
 
-window.onload = startGame();
+    function resetUI(){
+        instructionsElem.textContent = "Press any letter key to get started!"
+        numGuessesElem.textContent = numGuesses;
+        wrongLettersElem.textContent = "Wrong Letters: "
+        lowTriesElem.style.backgroundColor = "black";
+    }
+   
+    
+
 
 // User chooses a key
 document.onkeyup = function (event) {
+
     var playerGuess = event.key.toUpperCase();
     console.log(playerGuess);
-    
-   
 
     // Check if the key entered is a letter
     if (alphabet.includes(playerGuess.toLowerCase())) {
@@ -90,23 +67,38 @@ document.onkeyup = function (event) {
                 //use them to update the blank array
                 for (j = 0; j < guessIndex.length; j++){
                     var temp = guessIndex[j];
-                    console.log(blankWord[temp]);
                     blankWord[temp] = playerGuess;
                 }
 
                 // update the html and give user feedback
                 instructionsElem.innerHTML = "Good Job!"
                 missingLettersElem.innerHTML = blankWord.join(" ");
+
+                if (blankWord.join("") == wordToGuess){
+                    numWins++;
+                    numWinsElem.innerHTML= numWins;
+                    instructionsElem.innerHTML = "Great, Let's play again!"
+                    HangmanGame();
+
+                }
             
             } else {
 
                 wrongLetters.push(playerGuess);
-                wrongLettersElem.innerHTML = "Wrong Letters: " + wrongLetters.join(" ")
+                wrongLettersElem.innerHTML = "Wrong Letters: " + wrongLetters.join(" ");
                 numGuesses--;
+                instructionsElem.innerHTML = "Oops! Try another letter."
                 numGuessesElem.innerHTML = numGuesses;
 
                 // if no tries left - restart
-                updateLosses();
+                if (numGuesses == 0) {
+                    numLosses++;
+                    numLossesElem.innerHTML = numLosses;
+
+                    HangmanGame();
+                } else if(numGuesses <= 3){
+                    lowTriesElem.style.backgroundColor = "red";
+                }
             
             }
         
@@ -120,6 +112,25 @@ document.onkeyup = function (event) {
     else {
         console.log("not a letter");
         instructionsElem.innerHTML = "That is not a letter."
+    }
+
+}
+
+    function createBlanks(aString) {
+        for (i = 0; i < aString.length; i++) {
+            blankWord.push("_")
+        }
+        return blankWord
+    }
+
+
+    function findIndicies(aWord, character) {
+        var indicies = [], i;
+        for (i = 0; i < aWord.length; i++) {
+            if (aWord[i] === character)
+                indicies.push(i);
+        }
+        return indicies;
     }
 
 }
